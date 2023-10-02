9023b335b7ffe2a0025066647d51646c13bd39a4
x417v <ilyakinskiy@gmail.com>
2023-10-02 23:02:05 +0300

    add get,post,delete methods for categories, productrs, users (with jwt)

---
+++ helpers/jwt.js
@@ -0,0 +1,25 @@
+const { expressjwt: jwt } = require('express-jwt');
+
+function authJwt() {
+  const secret = process.env.secret;
+  const api = process.env.API_URL;
+  return jwt({
+    secret,
+    algorithms: ['HS256'],
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      { url: /\/api\/v1\/products{.*}/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/categories{.*}/, methods: ['GET', 'OPTIONS'] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+    ],
+  });
+}
+
+async function isRevoked(req, jwt) {
+  const payload = jwt.payload;
+  return !payload.isAdmin;
+}
+
+module.exports = authJwt;
